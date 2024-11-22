0e91a2954f8cd90c3a8ed139b6c1a06b8eb4bea0
Tamás Gavrán <b0gavtam@vasvari.org>
2024-11-22 11:03:01 +0100

    ConfigureWebHost

---
+++ PipeLine/Extensions/WebHostExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace PipeLine.Extensions
+{
+    public static class WebHostExtension
+    {
+        public static void ConfigureWebHost(this WebApplicationBuilder webApplicationBuilder)
+        {
+            webApplicationBuilder.WebHost.UseUrls("https://0.0.0.0/7020");
+        }
+    }
+}
